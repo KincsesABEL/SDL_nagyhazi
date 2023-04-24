7658fb79c9c79f092e6439e12fae14db066328a2
KincsesABEL <96739780+KincsesABEL@users.noreply.github.com>
2023-04-24 11:21:22 +0200

    Update SDL_Objektum.cpp

--- SDL_Objektum.cpp
+++ SDL_Objektum.cpp
@@ -1,42 +1,40 @@
-//Kincses Ábel SORHOJ
-// Created by kabel on 2023.04.17..
+//
+// Created by kabel on 2023.04.21..
 //
 
 #include "SDL_Objektum.h"
-#include <stdexcept>
-#include <string>
-#include <iostream>
+#include "SDL_Error.h"
 
-using namespace std;
-
-int SDL_Objektum::objektumszamlalo = 0;
+namespace SDL {
+    int SDL_Objektum::objektumszamlalo = 0;
 
 //SDL indítása
-void SDL_Objektum::indit() {
-    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0){
-        throw domain_error("SDL nem tudott elindulni! SDL error:" + string(SDL_GetError()));
-    }
+    void SDL_Objektum::indit() {
+        if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0){
+            throw SDL::SDL_Error("SDL nem tudott elindulni!");
+        }
 
-    if(!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
-        cout << "W: lineáris képszűrés nem engedélyezett" << endl;
+        if(!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
+            SDL_Log("W: lineáris képszűrés nem engedélyezett\n");
+        }
     }
-}
 
 //kilépés az SDL-ből
-void SDL_Objektum::bezar() {
-    SDL_Quit();
-}
+    void SDL_Objektum::bezar() {
+        SDL_Quit();
+    }
 
 //az első class indítja az SDL-t
-SDL_Objektum::SDL_Objektum() {
-    if(objektumszamlalo == 0) indit();
+    SDL_Objektum::SDL_Objektum() {
+        if(objektumszamlalo == 0) indit();
 
-    objektumszamlalo++;
-}
+        objektumszamlalo++;
+    }
 
 //az utolsó pedig kilép belőle
-SDL_Objektum::~SDL_Objektum() {
-    objektumszamlalo--;
+    SDL_Objektum::~SDL_Objektum() {
+        objektumszamlalo--;
 
-    if(objektumszamlalo == 0) bezar();
-}
+        if(objektumszamlalo == 0) bezar();
+    }
+} // SDL
