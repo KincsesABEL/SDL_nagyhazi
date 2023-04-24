8bcfd5d4f9e8993d13710113d6c9467ccdc8b397
KincsesABEL <96739780+KincsesABEL@users.noreply.github.com>
2023-04-24 11:19:09 +0200

    Update Ablak.h

--- Ablak.h
+++ Ablak.h
@@ -1,56 +1,67 @@
-//Kincses Ábel
-// Created by kabel on 2023.04.17..
+//
+// Created by kabel on 2023.04.21..
 //
 
-#ifndef SDL_ATIRAT_ABLAK_H
-#define SDL_ATIRAT_ABLAK_H
+#ifndef SDL_ATIRAT2_0_ABLAK_H
+#define SDL_ATIRAT2_0_ABLAK_H
 
+
+#ifdef _WIN32
+#include <SDL.h>
+#else
+#include <SDL2/SDL.h>
+#endif
 
 #include <string>
 #include "SDL_Objektum.h"
 
-using namespace std;
+namespace SDL {
 
-class Ablak : private SDL_Objektum {
-public:
-    explicit Ablak(string nev = "SDL ablak", int szelesseg = 640, int magassag = 480, bool allandomeret = true, bool szinkronizaltrenderer = true);
-    ~Ablak();
+    class Ablak : private SDL_Objektum {
+    public:
+        explicit Ablak(const std::string& nev = "SDL ablak", int szelesseg = 640, int magassag = 480, bool allandomeret = true, bool szinkronizaltrenderer = true);
+        ~Ablak();
 
 //    Ablakot másolni nem lehet, átadni is csak referenciában vagy pointerrel
-    Ablak(const Ablak&) = delete;
-    Ablak& operator= (const Ablak&) = delete;
+        Ablak(const Ablak&) = delete;
+        Ablak& operator= (const Ablak&) = delete;
 
-    void handleEvent(SDL_Event& e);
+        void handleEvent(SDL_Event& e);
 
-    void torol(const SDL_Color& szin = {0xFF, 0xFF, 0xFF, 0xFF});
-    void rajzol(){ SDL_RenderPresent(renderer);}
+        void torol(const SDL_Color& szin = {0xFF, 0xFF, 0xFF, 0xFF});
+        void rajzol(){ SDL_RenderPresent(renderer);}
 
-    void setRenderColor(const SDL_Color& szin = {0xFF, 0xFF, 0xFF, 0xFF});
+        void setRenderColor(const SDL_Color& szin = {0xFF, 0xFF, 0xFF, 0xFF});
 
 //    A renderer getterfüggvényéhez praktikus a - operátor használata
-    SDL_Renderer *getRenderer(){ return renderer;}
-    SDL_Renderer *operator-(){ return renderer;}
+        SDL_Renderer *getRenderer(){ return renderer;}
+        SDL_Renderer *operator-(){ return renderer;}
 
-    [[nodiscard]] const Uint32& getAblakAzonosito() const{ return azonositoszam;}
-    [[nodiscard]] const int& getSzelesseg() const{ return szelesseg;}
-    [[nodiscard]] const int& getMagassag() const{ return magassag;}
-    [[nodiscard]] const bool& egerRajtaVan_e() const{ return eger_rajta;}
-    [[nodiscard]] const bool& billentyuzetRajtaVan_e() const{ return billentyuzet_rajta;}
-    [[nodiscard]] const bool& teljesKepernyo_e() const{ return teljes_kepernyo;}
-    [[nodiscard]] const bool& talcaraTettek_e() const{ return eltuntetett;}
-    [[nodiscard]] const bool& latszik_e() const{ return latszik;}
+//    végtelen sok getterfüggvény
+        [[nodiscard]] Uint32 getAblakAzonosito() const{ return azonositoszam;}
+        [[nodiscard]] int getSzelesseg() const{ return szelesseg;}
+        [[nodiscard]] int getMagassag() const{ return magassag;}
+        [[nodiscard]] bool egerRajtaVan_e() const{ return eger_rajta;}
+        [[nodiscard]] bool billentyuzetRajtaVan_e() const{ return billentyuzet_rajta;}
+        [[nodiscard]] bool teljesKepernyo_e() const{ return teljes_kepernyo;}
+        [[nodiscard]] bool talcaraTettek_e() const{ return eltuntetett;}
+        [[nodiscard]] bool latszik_e() const{ return latszik;}
 
-private:
-    SDL_Window* ablakpointer;
-    SDL_Renderer* renderer;
+        SDL_Window* getAblakPointer(){ return ablakpointer;}
 
-    Uint32 azonositoszam;
+    private:
+        SDL_Window* ablakpointer;
+        SDL_Renderer* renderer;
 
-    int szelesseg, magassag;
+        Uint32 azonositoszam;
 
-    bool eger_rajta, billentyuzet_rajta, teljes_kepernyo, eltuntetett, latszik;
+        int szelesseg, magassag;
 
-    static int ablakszamlalo;
-};
+        bool eger_rajta, billentyuzet_rajta, teljes_kepernyo{}, eltuntetett{}, latszik;
 
-#endif //SDL_ATIRAT_ABLAK_H
+        static int ablakszamlalo;
+    };
+
+} // SDL
+
+#endif //SDL_ATIRAT2_0_ABLAK_H
